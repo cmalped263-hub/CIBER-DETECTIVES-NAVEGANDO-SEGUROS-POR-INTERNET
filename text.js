8e840ba8a586283271e8b09098d572b31254808a
cmalped263-hub <cmalped263@g.educaand.es>
2026-04-07 21:07:14 +0200

    Add files via upload
    subiendo web desde cero

---
+++ text.js
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es" id="exe-page-1775584885297-8g95rgg83">
+<head>
+<meta charset="utf-8">
+<meta name="generator" content="eXeLearning v4.0.0-rc1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="license" type="text/html" href="https://creativecommons.org/licenses/by-sa/4.0/">
+<title>6.1. Elegimos y creamos los instrumentos de evaluación</title>
+<link rel="icon" type="image/x-icon" href="../libs/favicon.ico">
+<meta name="description" content="Esta guía tiene como objetivo que el alumnado sepa proteger sus datos y derechos en la red. A través del
+eXeLearning tendrán a mano una selección de los mejores materiales para investigar y aprenderán a
+gestionar problemas técnicos comunes (como la pérdida de conexión), aplicando protocolos de guardado y trabajo offline.">
+<script>document.querySelector("html").classList.add("js");</script><script src="../libs/jquery/jquery.min.js"> </script><script src="../libs/common_i18n.js"> </script><script src="../libs/common.js"> </script><script src="../libs/exe_export.js"> </script><script src="../libs/bootstrap/bootstrap.bundle.min.js"> </script><link rel="stylesheet" href="../libs/bootstrap/bootstrap.min.css">
+<script src="../idevices/text/text.js"></script><link rel="stylesheet" href="../idevices/text/text.css">
+<link rel="stylesheet" href="../content/css/base.css"><script src="../theme/style.js"> </script><link rel="stylesheet" href="../theme/style.css">
+</head>
+<body class="exe-export exe-web-site">
+<script>document.body.className+=" js"</script>
+<div class="exe-content exe-export pre-js siteNav-hidden"><a href="#page-1775584885297-8g95rgg83" id="skipNav">Skip to content</a> <nav id="siteNav">
+<ul>
+<li> <a href="../index.html" class="main-node no-ch">.</a>
+</li>
+<li> <a href="../html/1-recursos-educativos-abiertos.html" class="daddy">1. RECURSOS EDUCATIVOS ABIERTOS</a>
+<ul class="other-section">
+<li> <a href="../html/11-comenzamos-nuestro-proyecto.html" class="no-ch">1.1. Comenzamos nuestro proyecto</a>
+</li>
+<li> <a href="../html/12-licencias-y-seguridad.html" class="no-ch">1.2. Licencias y seguridad</a>
+</li>
+</ul>
+</li>
+<li> <a href="../html/2-herramientas-para-la-creacion-y-publicacion-de-c.html" class="daddy">2. HERRAMIENTAS PARA LA CREACIÓN Y PUBLICACIÓN DE CONTENIDO</a>
+<ul class="other-section">
+<li> <a href="../html/21-elegimos-la-herramienta-para-el-producto-final.html" class="no-ch">2.1. Elegimos la herramienta para el producto final</a>
+</li>
+</ul>
+</li>
+<li> <a href="../html/3-herramientas-para-la-organizacion-y-la-gestion-d.html" class="daddy">3. HERRAMIENTAS PARA LA ORGANIZACIÓN Y LA GESTIÓN DE LA INFORMACIÓN</a>
+<ul class="other-section">
+<li> <a href="../html/31-curacion-de-contenido.html" class="no-ch">3.1. Curación de contenido</a>
+</li>
+</ul>
+</li>
+<li> <a href="../html/4-organizacion-temporal-espacial-y-grupal.html" class="daddy">4. ORGANIZACIÓN TEMPORAL, ESPACIAL Y GRUPAL</a>
+<ul class="other-section">
+<li> <a href="../html/41-linea-temporal-para-la-organizacion.html" class="no-ch">4.1. Línea temporal para la organización</a>
+</li>
+</ul>
+</li>
+<li> <a href="../html/5-guia-didactica.html" class="daddy">5. GUÍA DIDÁCTICA</a>
+<ul class="other-section">
+<li> <a href="../html/51-asociamos-tareas-a-criterios-de-evaluacion.html" class="no-ch">5.1. Asociamos tareas a criterios de evaluación</a>
+</li>
+</ul>
+</li>
+<li class="current-page-parent"> <a href="../html/6-evaluacion.html" class="daddy">6. EVALUACIÓN</a>
+<ul class="other-section">
+<li class="active"> <a href="../html/61-elegimos-y-creamos-los-instrumentos-de-evaluaci.html" class="active no-ch">6.1. Elegimos y creamos los instrumentos de evaluación</a>
+</li>
+</ul>
+</li>
+<li> <a href="../html/7-creditos.html" class="no-ch">7. CRÉDITOS</a>
+</li>
+</ul>
+</nav><main id="page-1775584885297-8g95rgg83" class="page"> 
+<header class="main-header">
+<div class="package-header"><p class="package-title">CIBER DETECTIVES: NAVEGANDO SEGUROS POR INTERNET</p>
+<p class="package-subtitle">Guía interactiva de supervivencia digital</p></div>
+<div class="page-header"><h1 class="page-title">6.1. Elegimos y creamos los instrumentos de evaluación</h1></div>
+</header><div id="page-content-page-1775584885297-8g95rgg83" class="page-content">
+<article id="block-1775584925231-amw6cd8t2" class="box no-header">
+<header class="box-head no-icon">
+<button class="box-toggle box-toggle-on" title="Toggle content">
+<span>Toggle content</span>
+</button></header>
+<div class="box-content">
+<div id="idevice-1775584925230-s8ks248h2" class="idevice_node text" data-idevice-path="../idevices/text/" data-idevice-type="text" data-idevice-component-type="json" data-idevice-json-data="{&quot;ideviceId&quot;:&quot;idevice-1775584925230-s8ks248h2&quot;}">
+<div class="exe-text-template">
+    <p>Los dos instrumentos de evaluación que voy a usar para la evaluación de mi alumnado son los siguientes:<br />En primer lugar, voy a realizar una evaluación inicial e individual, en la primera de las tareas utilizaré como instrumento una encuesta. Con ella evaluaré los conocimientos previos de mi alumnado sobre ciberseguridad. Los resultados que espero obtener son tanto cuantitativos como cualitativos. Lo analizaría atendiendo a las gráficas que la propia herramienta tiene tras la realización de dicho formulario. En ellas se puede observar datos cuantitativos con su también correspondiente interpretación cualitativa sobre lo que estamos analizando.<br />Resultados que me servirán para conocer el punto de partida de este proyecto, qué cosas conocen y cuáles deben aprender. Del análisis de estos resultados se pueden obtener conclusiones sobre la elección y el diseño de los instrumentos o de la tarea en sí misma y podemos tomar medidas para mejorarlos en futuras implementaciones. Este es el formulario: <a href="https://docs.google.com/forms/d/e/1FAIpQLSeW7WCRxBRIug50IcNMaRToVBZHwj9Akn0iWWuUaOwgNvamfQ/viewform">https://docs.google.com/forms/d/e/1FAIpQLSeW7WCRxBRIug50IcNMaRToVBZHwj9Akn0iWWuUaOwgNvamfQ/viewform</a> </p>
+<p>En segundo lugar, voy a usar una rúbrica como instrumento de evaluación del proyecto. La usaré durante el proceso (evaluación formativa) y al finalizar el proyecto (evaluación final).<br />Recogeré los datos mediante la observación directa, en la evaluación del producto final (Genially) y con el registro en cuaderno del docente.<br />Los resultados me servirán para realizar modificaciones o no en la mejora del proceso de enseñanza-aprendizaje. La rúbrica me permite evaluar de forma criterial y objetiva el desempeño del alumnado, facilitando una retroalimentación clara y orientada a la mejora, en coherencia con el enfoque competencial de la LOMLOE.</p>
+<p>Enlace a la rúbrica: <a href="https://drive.google.com/file/d/1W9ahO4DNtDK8KZ5ilHgwvzIhpTuYEDwu/view">https://drive.google.com/file/d/1W9ahO4DNtDK8KZ5ilHgwvzIhpTuYEDwu/view</a> <br /></p>
+</div>
+</div>
+</div>
+</article>
+</div></main><div class="nav-buttons">
+<a href="../html/6-evaluacion.html" title="Anterior" class="nav-button nav-button-left"><span>Anterior</span></a>
+<a href="../html/7-creditos.html" title="Siguiente" class="nav-button nav-button-right"><span>Siguiente</span></a>
+</div>
+<footer id="siteFooter"><div id="siteFooterContent"> <div id="packageLicense" class="cc cc-by-sa"> <p> <span class="license-label">Licencia: </span><a href="https://creativecommons.org/licenses/by-sa/4.0/" class="license">creative commons: attribution - share alike 4.0 (BY-SA)</a></p>
+</div>
+</div></footer>
+</div>
+<p id="made-with-eXe"> <a href="https://exelearning.net/" target="_blank" rel="noopener"> <span>Creado con eXeLearning <span>(nueva ventana)</span></span></a></p>
+</body>
+</html>
